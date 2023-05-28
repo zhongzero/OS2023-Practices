cd7eba4fb2b9754dcf61b99c833974769bf3dfaa
zhongzero <1144265105@qq.com>
2023-05-28 17:06:34 +0800

    update my_chmod

--- practice-3/filesystem/fuse-3.14.1/example/my_bot.c
+++ practice-3/filesystem/fuse-3.14.1/example/my_bot.c
@@ -387,6 +387,7 @@ static int my_chmod(const char *path, mode_t mode, struct fuse_file_info *fi){
 	struct File *file=FindFile(path);
 	if(file==NULL)return -ENOENT;
 	file->vstat.st_mode=mode;
+	file->vstat.st_ctime = time(NULL);	// 上次文件状态修改时间
 	return 0;
 }
 
@@ -451,7 +452,7 @@ int main(int argc, char* argv[]) {
 //     unsigned long st_blocks;    //块数
 //     time_t        st_atime;     //最后一次访问时间
 //     time_t        st_mtime;     //最后一次修改时间
-//     time_t        st_ctime;     //最后一次改变时间(指属性)
+//     time_t        st_ctime;     //上次文件状态修改时间
 // };
 
 // getattr() 类似于stat()
